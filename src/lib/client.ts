dff05cf1ee221002e1a6702a0f16f51cc6419d15
zahid <z.emran@flowqi.com>
2026-02-27 21:42:49 +0600

    env variable name prefixed with NEXT_PUBLIC_ as it was used from the browser environment

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -2,4 +2,4 @@ import { treaty } from '@elysiajs/eden'
 import type { App} from '@/app/api/[[...slugs]]/route'
 
 
-export const client = treaty<App>(`${process.env.FRONTEND_URL}`).api
+export const client = treaty<App>(`${process.env.NEXT_PUBLIC_FRONTEND_URL}`).api
